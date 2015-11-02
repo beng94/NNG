3df1e318f708089df0dd8bd3c2a814ab3f2707e8
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-11-02 20:07:54 +0100

    3_dna: Testing program added

---
+++ 3_dns/test.py
@@ -0,0 +1,12 @@
+f_dns = open("dns.txt", 'r')
+f_decoded = open("decoded_dns.txt", 'r')
+
+dns_str = f_dns.readline()
+decoded_str = f_decoded.readline()
+
+print len(dns_str)
+print len(decoded_str)
+
+for id_ in range(len(dns_str)):
+    if dns_str[id_] != decoded_str[id_]:
+        print "Fuck that", id_, dns_str[id_], decoded_str[id_], dns_str[id_ - 8: id_ + 8]
