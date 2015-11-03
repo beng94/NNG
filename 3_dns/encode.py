e2bc6d902accae24e75e67d075b5113ce463e2be
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-11-03 20:39:24 +0100

    3_dna: Encoding program updated to use Python 3.5

--- 3_dns/encode.py
+++ 3_dns/encode.py
@@ -6,7 +6,7 @@ def char_to_bin (char):
         'T': '11'
     } [char]
 
-f_o = open("encoded_dns.txt", 'w')
+f_o = open("encoded_dns.txt", 'wb')
 #f_o.write(chr(2).decode('ascii'))
 with open("dns.txt", 'r') as f:
     dna_string = f.readline()
@@ -21,11 +21,9 @@ with open("dns.txt", 'r') as f:
 
         #UTF-32 max 21 bit
         if len(utf_char) == 8:
-            print utf_char, int(utf_char, 2), unichr(int(utf_char, 2)).encode('latin-1')
             #f_o.write(utf_char + " ")
             #f_o.write(str(int(utf_char, 2)) + " ")
-            f_o.write(unichr(int(utf_char, 2)).encode('latin-1'))
+            f_o.write(chr(int(utf_char, 2)).encode('latin-1'))
             utf_char = ""
 
 f_o.close()
-
