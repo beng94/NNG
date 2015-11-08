24250d3d271aa9cc73a3c11d61aecb60b2996323
Gedai Tamás Bence <gedai.bence@gmail.com>
2015-11-08 21:04:37 +0100

    3_dns: HTTP hack implemented

---
+++ 3_dns/HTTP_hack.cpp
@@ -0,0 +1,26 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <tchar.h>
+#include <urlmon.h>
+#pragma comment(lib, "urlmon.lib")
+
+using namespace std;
+
+int main()
+{
+	const TCHAR url[] = _T("http://ge.tt/api/1/files/77h0hRR2/0/blob?download");
+	const TCHAR location[] = _T("dns.txt");
+
+	cout << "downloading Piriform CCleaner...";
+	HRESULT hr = URLDownloadToFile(NULL, url, location, 0, NULL);
+
+	std::ifstream file("dns.txt");
+	std::string dns;
+
+	file >> dns;
+	std::cout << dns << std::endl;
+	file.close();
+}
