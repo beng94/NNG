b4c5892b5e4df77afbd712fdf193555bc8635a3d
MoriczTamas <mtamas@mensa.hu>
2015-11-15 00:22:37 +0100

    1_vonat: new islandsort

--- 1_vonat/main.cpp
+++ 1_vonat/main.cpp
@@ -1,16 +1,21 @@
 #include "crane.h"
 #include <iostream>
+#include <algorithm>
 
 int main(){
 	//Pájnusz monochrome perpetuity
-	Crane c("hcedffhcjjhgbgjiiadg", "jhhhgbcjffdecgjgdaii");
-	std::cout << c.stringflip("aaabbbccc", 1, 3) << std::endl;
+	Crane c("hcedffhcjjhgbgjiiadgecifjedjfffgedcbgaiijdciffgjaeihcdbgffagdjabcjecgbajdibjgdfbefdcadcjcjgdgbcdfeeacffidiijajgcafbijiaeifdcafjahhgffbhdajdccbadaejeehjdgaadefejhdijgdacbfjbgedeccjibhfiaaaibgffghhcfedigbcidigcaeigjgabgbdhiedghjjgfbeeajjgcihebfjaheajaddjacgbdgbdbdcjbbbjdhebaeheadaedcjcgehcjeeagcgbehhjicfbcjadfjefgeehdbdbdeggdaaifihagihhbdeehbeddbfdaabgfiefjjecaabgdebheadfchidaahieahjahaeidffcgcbachhighggifccidadfhhghhfjidageeigidajeggdhaacajiebjdchejdbfbicihbjgbchjabdeedfhceeaafccdjfjfgjdafddgaajeihfjbbfgcehfjhibjdfgcfgghijahfhbjjigacdgiebdeadbfjjjhgeabgaahjffchcjjaghaeedefdadijbaabadhdadhfeidbbgeiaiigijagabbjcgegcbcebaaiafhggajcfageeddiiaeahbaaijgdfcjggbbccghbbdhhfcfdhdjeifgjdjjjibfjgcjgdaibfejiibifcebijadebeefibajcbiibafejiacdefacdchidhfgjdhicdbedfjgfdaijfadhcdbjhaibjidgfigahggfcbjieeficjheeccgjfcgbihgbhjghjfgiccibbgghgigfegehjeiaiicjgdaeedhjibfiddcbfjdaghbaddbffeidghfiehfjjgdjjcjgadjgbjdeejbeagidjhgfeciedcddjafcejhdcbccejffcchhibceaijhcdhbciaehgagjgeidecjgffhbfijfeegheccajbdaajdddfgad",
+			"hcedffggjfbabjhaiabbijhhebgcgaeejchegjchjabcegdaiijgbgdiigdcacaihagihibjdfghbghibgcfjgcchaeidfeehjdgaiaaaijifchdajdghhacfddjfjgjcdfeieajhadfigjaajfgicfgdcchfiehfgigjbigjgagjiafhbjjjhfgbdcejbfacgeegaaajeibdbdihdebgaijefabiibcjcjcdacdfdhjefedaijddjaajdbajidajciiaajgfaabgciiijbdjfeeedeccjgaacbbggjfcjcibbggcjebaaiahdhacbgideifecfibibgffghhheijjhgeabfjbehbjgdifbcacfdhdjeifecffeidgfaaeegfhdheegeeibceaijhcdfaeheahfdbcddaffbijagabbigccajbdhbjgdfbjebdehjcifeebhecdeebgeiaigebdjcadhdahgfebjjgfajdecjchiabiciidiifbhcecidadfheihdbgihajaddjacgfhggajcfagejfejdbfgdjabcjecgbjibgbdbdbfhhdeggdaajjchcffiejfaecjdaeedhgegcjdaiaffhbifffgjjjhccffjeacbgdfedagjgieacfhaeebeiefedddcdcjjdgjeicgcbaheajdedabdffhdgggjdhgdabigadjccbcdhjjajhjafdeedgadhgaaeihjfhecjdeagifgdijbdhdbjgdgchhiedcbgachdfchidaahajhddiahbfijefbdghbgbhgjcfiajcfdiiccajdfadhcicdbedfjafcejfbdaebbbjbefgeedhebjhjhgfeciefjaeibehicgjjaeebdghjdcbcdfgbbhgifjdjgfdajfaccjghabcgdaibhbdebfgbchbaddbdgddgaffieaicbhifjhggiijdcifcejjfeafegefgighddegghijaheigdfjicf");
+	//std::cout << c.stringflip("aaabbbccc", 1, 3) << std::endl;
 	c.sort();
 	c.getresults();
-	std::string valami("abcdefgh");
+	/*std::string valami("abcdefgh");
 	std::cout << std::endl << std::string(valami.rbegin(), valami.rend()) << std::endl;
 	std::cout << valami.substr(2, 4) << std::endl;
 	std::cout << c.stringflip(valami, 2, 6);
-	getchar();
-	while (true){}
+	getchar();*/
+	/*std::string fasztalicskakettõ = "abcthzhjuk";
+	std::sort(fasztalicskakettõ.begin(), fasztalicskakettõ.begin() + 6);
+	std::cout << fasztalicskakettõ;*/
+	//while (true){}
 }
